44a73d1fedead5542da9b3827cc5061b3a1a571d
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-03-15 16:17:56 +0800

    update build scripts and meta-data

--- openssl-3/null-ptr-18/build.sh
+++ openssl-3/null-ptr-18/build.sh
@@ -6,19 +6,5 @@ bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=$1/$benchmark_name/$project_name/$bug_id
 cd $dir_name/src
 
-PROJECT_CFLAGS="-O0 -Wno-error -g"
-PROJECT_CXXFLAGS="-O0-Wno-error -g"
-PROJECT_LDFLAGS="-O0 -Wno-error -g"
-
-if [[ -n "${CFLAGS}" ]]; then
-  PROJECT_CFLAGS="${PROJECT_CFLAGS} ${CFLAGS}"
-fi
-if [[ -n "${CXXFLAGS}" ]]; then
-  PROJECT_CXXFLAGS="${PROJECT_CXXFLAGS} ${CXXFLAGS}"
-fi
-if [[ -n "${LDFLAGS}" ]]; then
-  PROJECT_LDFLAGS="${PROJECT_LDFLAGS} ${LDFLAGS}"
-fi
-
-make CFLAGS="${PROJECT_CFLAGS}" CXXFLAGS="${PROJECT_CXXFLAGS}" LDFLAGS="${PROJECT_LDFLAGS}" -j`nproc`
+make -j`nproc`
 
